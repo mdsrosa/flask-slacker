494fd9dd3cb526682e5cb6fabc12ce4263875aea
Matheus Rosa <matheusdsrosa@gmail.com>
2017-09-23 16:59:17 +0100

    Load app configs for Slacker

--- flask_slacker/__init__.py
+++ flask_slacker/__init__.py
@@ -7,14 +7,14 @@
     :copyright: (c) 2017 Matheus Rosa
     :license: MIT, see LICENSE for more details.
 """
-from slacker import Slacker as BaseSlacker
+from slacker import Slacker as BaseSlacker, DEFAULT_TIMEOUT
 
 
 __version__ = '0.0.1'
 
 
 class Slacker(object):
-    def __init__(self, app=None, **kwargs):
+    def __init__(self, app=None):
         """Initialize the Slacker interface.
 
         :param app: Flask application
@@ -22,13 +22,18 @@ class Slacker(object):
         if app is not None:
             self.init_app(app)
 
-    def init_app(self, app, config=None):
+    def init_app(self, app):
         """
         Initialize the app in Flask.
         """
-        if not (config is None or isinstance(config, dict)):
-            raise ValueError("`config` must be an instance of dict or None")
+        app.config.setdefault('SLACKER_TIMEOUT', DEFAULT_TIMEOUT)
+
+        if 'SLACKER_TOKEN' not in app.config:
+            raise Exception('Missing SLACKER_TOKEN in your config.')
+
+        token = app.config['SLACKER_TOKEN']
+        timeout = app.config['SLACKER_TIMEOUT']
 
         # register application within app
         app.extensions = getattr(app, 'extensions', {})
-        app.extensions['slack'] = BaseSlacker(**config)
+        app.extensions['slack'] = BaseSlacker(token, timeout=timeout)
