32b7c2ab5b69772e57fa028b27b57c18fcb35436
Matheus Rosa <matheusdsrosa@gmail.com>
2018-10-07 20:10:33 +0100

    Fix formatting in flask_slacker package

--- flask_slacker/__init__.py
+++ flask_slacker/__init__.py
@@ -10,7 +10,7 @@
 from slacker import Slacker as BaseSlacker, DEFAULT_TIMEOUT
 
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
 
 class Slacker(object):
@@ -26,30 +26,36 @@ class Slacker(object):
         """
         Initialize the app in Flask.
         """
-        app.config.setdefault('SLACKER_TIMEOUT', DEFAULT_TIMEOUT)
-        app.config.setdefault('SLACKER_HTTP_PROXY', None)
-        app.config.setdefault('SLACKER_HTTPS_PROXY', None)
+        app.config.setdefault("SLACKER_TIMEOUT", DEFAULT_TIMEOUT)
+        app.config.setdefault("SLACKER_HTTP_PROXY", None)
+        app.config.setdefault("SLACKER_HTTPS_PROXY", None)
 
         if config is None:
             config = app.config
 
-        if token is None and 'SLACKER_TOKEN' not in config:
-            raise Exception('SLACKER_TOKEN not found in your config '
-                            'nor `token` was informed.')
+        if token is None and "SLACKER_TOKEN" not in config:
+            raise Exception(
+                "SLACKER_TOKEN not found in your config " "nor `token` was informed."
+            )
 
-        token = config['SLACKER_TOKEN'] or token
-        timeout = config['SLACKER_TIMEOUT']
-        http_proxy = config['SLACKER_HTTP_PROXY']
-        https_proxy = config['SLACKER_HTTPS_PROXY']
+        token = config["SLACKER_TOKEN"] or token
+        timeout = config["SLACKER_TIMEOUT"]
+        http_proxy = config["SLACKER_HTTP_PROXY"]
+        https_proxy = config["SLACKER_HTTPS_PROXY"]
 
-        self._slacker = BaseSlacker(token, timeout=timeout, http_proxy=http_proxy,
-                                    https_proxy=https_proxy, session=session)
+        self._slacker = BaseSlacker(
+            token,
+            timeout=timeout,
+            http_proxy=http_proxy,
+            https_proxy=https_proxy,
+            session=session,
+        )
 
         # set Slacker attributes
-        attrs = filter(lambda a: '_' not in a, dir(self._slacker))
+        attrs = filter(lambda a: "_" not in a, dir(self._slacker))
         for attr in attrs:
             setattr(self, attr, getattr(self._slacker, attr))
 
         # register application within app
-        app.extensions = getattr(app, 'extensions', {})
-        app.extensions['slacker'] = self
+        app.extensions = getattr(app, "extensions", {})
+        app.extensions["slacker"] = self
